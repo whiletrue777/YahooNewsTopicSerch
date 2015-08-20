5bc4f6f1a2528a596eb5a6774ee12ff906f081ab
GOUDA <4line.waon@gmail.com>
2015-08-20 04:52:44 +0000

    sample2

--- features/step_definitions/serchSteps.rb
+++ features/step_definitions/serchSteps.rb
@@ -4,9 +4,10 @@ end
 
 ならば(/^ページタイトルが"([^"]*)"を表示すること$/) do |title|
  assert_title($title)
- file = File.open("../log.txt", "a")
- file.puts all('span.ttl')[0].text(:all)
- file.puts Time.now.strftime("%X")+"\n"
- file.close
+end
+
+ならば(/^アクセスランキング一位のトピック名を取得すること$/) do
+  puts all('span.ttl')[0].text(:all)
+  puts Time.now.strftime("%X")+"\n"
 end
 
